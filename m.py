af728b8d5e9922fd8d8192216aada47924ac6885
Kelares <michalowski.jb@gmail.com>
2025-05-12 18:28:03 +0200

    Clean up

--- m.py
+++
@@ -1,4 +0,0 @@
-import max7219.led as led
-
-device = led.matrix()
-device.show_message("Hello world!")
--- matrix.py
+++
@@ -1,6 +0,0 @@
-from luma.core.interface.serial import spi, noop
-from luma.core.render import canvas
-from luma.led_matrix.device import max7219
-
-serial = spi(port=0, device=0, gpio=noop())
-device = max7219(serial)
